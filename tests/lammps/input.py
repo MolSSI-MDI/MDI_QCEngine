86e442c9985871c7110c388ef6251a92d3a93684
Taylor Barnes <taylor.a.barnes@gmail.com>
2023-04-05 10:36:31 -0400

    Add LAMMPS to build

---
+++ tests/lammps/input.py
@@ -0,0 +1,27 @@
+import os
+import qcengine as qcng
+import qcelemental as qcel
+
+mol = qcel.models.Molecule.from_data("""
+O  0.0  0.000  -0.0683
+H  0.0 -0.791  0.543
+H  0.0  0.791  0.543
+""")
+
+o = qcng.MDIServer(mdi_options = os.getenv('-role ENGINE -name QM -method TCP -port 8021 -hostname localhost'),
+                   program = "gamess",
+                   molecule = mol,
+                   model={"method": "SCF", "basis": "STO"},
+                   keywords={"basis__ngauss": "3",
+                             "scf__dirscf": ".TRUE.", 
+                             "guess__guess": "HUCKEL", 
+                             "contrl__units": "BOHR",
+                             "contrl__maxit": "60"}
+                  )
+o.start()
+
+
+#print("Hello")
+#print("ret: ",str(ret))
+#print("Energy: " + str(ret.return_result))
+##print("ERROR: ", str(ret.error.error_message))
